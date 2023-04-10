174866ca17f7500e9ea8f5f2a79796d89d5a75be
BonsaiiV <dominik.riemer@udo.edu>
2023-04-10 18:30:33 +0200

    included nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,10 @@
+local opt = vim.opt
+
+opt.number		= true
+opt.relativenumber	= true
+opt.autoindent		= true
+opt.tabstop		= 4
+opt.shiftwidth		= 4
+opt.smarttab		= true
+opt.softtabstop		= 4
+opt.mouse		= a
