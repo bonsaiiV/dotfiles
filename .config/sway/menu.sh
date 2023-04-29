735ff6b7dabb19aedad4ddd0b24a04a6661148cf
= <=>
2023-04-29 13:57:23 +0200

    new menu design

--- .config/sway/menu.sh
+++ .config/sway/menu.sh
@@ -2,7 +2,7 @@
 find -L $(sed 's/:/\/applications /g' <<< "$XDG_DATA_HOME:$XDG_DATA_DIRS:") -type f -name \*.desktop \
     | xargs gawk -f ~/.config/sway/parse-desktop-entry.awk \
     | sort \
-    | gawk -F "\t" -v menu='bemenu -ipλ' '
+    | gawk -F "\t" -v menu='bemenu -ipλ --ab="#303030" --fb="#303030" --tb="#F4F4F4" --hb="#993399" --hf="#FFFFFF" --nb="#303030"' '
         {
             print $1 |& menu
             cmds[$1] = $2
