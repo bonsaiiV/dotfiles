37dd31b1cec4e59928adccb9717058455f6e8c75
Bonsaiiv <dominik.riemer@udo.edu>
2023-05-12 14:22:46 +0200

    set fish as default shell for alacritty

---
+++ .config/fish/conf.d/aliases.fish
@@ -0,0 +1,11 @@
+alias open='xdg-open'
+alias ll='ls -All'
+alias doas='doas '
+alias vi='nvim'
+alias vim='nvim'
+alias pacinstall='doas pacman -S'
+alias uninstall='doas pacman -Rsu'
+alias pacupgrade='doas pacman -Sy && doas pacman -S --needed --noconfirm archlinux-keyring && doas pacman -Su'
+
+alias sudo='sl -d10lFaw'
+alias xprop='echo "This is sway, try: \"swaymsg -t get_tree\""'
