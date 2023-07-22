c61dda6cc6baacfa05a8acaeb62ffe8fefe3fda2
Bonsaiiv <dominik.riemer@udo.edu>
2023-07-22 14:32:45 +0200

    restructure fishconfig

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-export MOZ_ENABLE_WAYLAND=1
-export EDITOR='nvim'
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_DATA_DIRS="/usr/local/share:/usr/share"
 export XDG_CONFIG_HOME="$HOME/.config"
+
+export MOZ_ENABLE_WAYLAND=1
+export EDITOR='nvim'
 export JAVA_HOME="/usr"
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
 #if test -f $HOME/.config/fish/display.fish
 #	$HOME/.config/fish/display.fish
 #end
