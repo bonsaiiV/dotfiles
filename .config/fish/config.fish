31af3348b1d87475561f8028853c9672491bd4a7
Bonsaiiv <dominik.riemer@udo.edu>
2023-10-27 19:53:08 +0200

    fixed env

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,15 +2,8 @@ if status is-interactive
 	set -U fish_greeting
 	fish_config theme choose Dracula
 end
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_DATA_DIRS="/usr/local/share:/usr/share"
-export XDG_CONFIG_HOME="$HOME/.config"
 
-export MOZ_ENABLE_WAYLAND=1
-export EDITOR='nvim'
-
-export JAVA_HOME="/usr"
-export CARGO_HOME="$XDG_DATA_HOME/cargo"
+#export EDITOR='nvim'
 
 export CABAL_CONFIG="$XDG_CONFIG_HOME/cabal/config"
 
