87fcca84d12d24d44f60dc9c0ceb0d97bb7bba5b
Bonsaiiv <dominik.riemer@udo.edu>
2024-02-05 14:08:00 +0100

    added pacman.conf added flag so files are only installed if changed added lesshistfile location to pam_env

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -5,5 +5,6 @@ set -eu
 path=${0%/*}
 cd "$path/system_files/"
 
-install -Dm644 --backup ./pam_env.conf -t /etc/security/
-install -Dm644 --backup ./custom_xkb -t /usr/share/X11/xkd/symbols/
+install -DCm644 --backup ./pacman.conf -t /etc/
+install -DCm644 --backup ./pam_env.conf -t /etc/security/
+install -DCm644 --backup ./custom_xkb -t /usr/share/X11/xkd/symbols/
