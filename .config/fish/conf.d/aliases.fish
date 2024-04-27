d67dd8576f7bc395305ac5f0e03af13e2281b2f6
Bonsaiiv <dominik.riemer@udo.edu>
2024-04-27 11:44:39 +0200

    uninstall alias now uses "pacman -Runs"

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -4,7 +4,7 @@ alias doas='doas '
 alias vi='nvim'
 alias vim='nvim'
 alias pacinstall='doas pacman -S'
-alias uninstall='doas pacman -Rsu'
+alias uninstall='doas pacman -Runs'
 alias pacupgrade='doas pacman -Sy && doas pacman -S --needed --noconfirm archlinux-keyring && doas pacman -Su'
 
 alias dotgit='git --git-dir={$HOME}/.dotfiles/.git --work-tree={$HOME}'
