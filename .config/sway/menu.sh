eae36e2b79926652a4ce6e7809b45d497904ec89
Bonsaiiv <dominik.riemer@udo.edu>
2024-04-27 11:48:06 +0200

    menu filtering with old bemenu

--- .config/sway/menu.sh
+++ .config/sway/menu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 find -L $(sed 's/:/\/applications /g' <<< "$XDG_DATA_HOME:$XDG_DATA_DIRS:") -type f -name \*.desktop 2> /dev/null \
-	| grep -v wine \
+	| grep -Ev "wine|electron|jdk|applications/Qt" \
     | xargs gawk -f ~/.config/sway/parse-desktop-entry.awk \
     | sort \
     | gawk -F "\t" -v menu='bemenu -ipλ --ab="#303030" --fb="#303030" --tb="#F4F4F4" --hb="#993399" --hf="#FFFFFF" --nb="#303030"' '
