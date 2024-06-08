30509a23d395a0551392603db5e462f744ff3094
Bonsaiiv <dominik.riemer@udo.edu>
2024-06-08 11:32:20 +0200

    refactor system_file install

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -5,11 +5,8 @@ set -eu
 path=${0%/*}
 cd "$path/system_files/"
 
-install -DCm644 --backup ./pacman.conf -t /etc/
-install -DCm644 --backup ./pam_env.conf -t /etc/security/
-install -DCm644 --backup ./custom_xkb -t /usr/share/X11/xkd/symbols/
-install -DCm644 --backup ./personal.kbd -t /etc/kmonad/
-install -DCm644 --backup ./70-kmonad.rules -t /etc/udev/rules.d/
-install -DCm644 --backup ./dotfiles-kmonad@.service -t /etc/systemd/system/
-install -DCm644 --backup ./sd-kmonad -t /etc/initcpio/install/
-install -DCm644 --backup ./mkinitcpio.conf -t /etc/
+targets="$(find -type f)"
+for target in $targets; do
+	location="$(sed	-e s/^.// <<< $target)"
+	install -DCm644 --backup "${target}" -T "${location}"
+done
