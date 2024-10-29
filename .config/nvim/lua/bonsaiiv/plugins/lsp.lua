1a2098e343304dc2d7703e9b1cfcc5c4b742ac65
Bonsaiiv <dominik.riemer@udo.edu>
2024-10-29 17:47:49 +0100

    nice diagnostics

--- .config/nvim/lua/bonsaiiv/plugins/lsp.lua
+++ .config/nvim/lua/bonsaiiv/plugins/lsp.lua
@@ -126,6 +126,27 @@ return {
 				'<Leader>d',
 				function() vim.diagnostic.open_float() end
 			)
+			vim.keymap.set(
+				'n',
+				'<Leader>a',
+				function()
+					local count = 0
+					local lines = {}
+					for k, diag in pairs(vim.diagnostic.get(0)) do
+						count = k
+						table.insert(lines, tostring(diag.lnum + 1) .. ": " .. diag.message)
+					end
+					if count == 0 then
+						lines = {"no diagnostics in file"}
+					end
+					local buf = vim.api.nvim_create_buf(false, true)
+					vim.api.nvim_buf_set_lines(buf, 0, 0, false, lines)
+					local win = vim.api.nvim_open_win(buf, true, {relative = 'win', row=3, col=10, width=100, height=12})
+					vim.wo[win].number = false
+					vim.wo[win].relativenumber = false
+					vim.keymap.set('n', '<ESC>', function() vim.api.nvim_win_close(win,false) end, {buffer = 0})
+				end
+			)
 			local lsp_zero = require 'lsp-zero'
 			lsp_zero.extend_lspconfig()
 			lsp_zero.on_attach(function(_, bufnr)
