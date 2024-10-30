7349e256eecfedc118f6e2061a41b809150c5dc5
Bonsaiiv <dominik.riemer@udo.edu>
2024-10-30 13:08:05 +0100

    print multiple files

--- .dotfiles/uni-druck.sh
+++ .dotfiles/uni-druck.sh
@@ -2,26 +2,29 @@
 printer="ohs14pr1"
 ssh_target="uran"
 while getopts ':p:' OPTION; do
-	case "$OPTION" in
-		p)
-			case "$OPTARG" in
+    case "$OPTION" in
+        p)
+            case "$OPTARG" in
 
-				"fsr")
-					printer=FSR-Buero
-					ssh_target=eins
-					;;
+                "fsr")
+                    printer=FSR-Buero
+                    ssh_target=eins
+                    ;;
 
-				"oh14")
-					printer=ohs14pr1
-					;;
+                "oh14")
+                    printer=ohs14pr1
+                    ;;
 
-			esac
-	esac
+            esac
+    esac
 done
 shift "$(($OPTIND -1))"
-if [ $# -lt 1 -a -e $1 ]; then
-	echo "file not specified or found"
-	exit 1
-fi
-echo "printing $1 at $printer accessing through $ssh_target"
-cat "$1" | ssh $ssh_target "lpr -P $printer"
+while [ $# -gt 0 ]; do
+    if [ ! -e "$1" ]; then
+        echo "file not found"
+        exit 1
+    fi
+    echo "printing $1 at $printer accessing through $ssh_target"
+    cat "$1" | ssh $ssh_target "lpr -P $printer"
+    shift 1
+done
