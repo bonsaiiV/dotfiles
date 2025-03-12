01a0d9f3ee15a1c16ef3fd7d4e419ab21da9b1d8
Bonsaiiv <dominik.riemer@udo.edu>
2025-03-12 15:45:51 +0100

    whitespace in printscript

--- .dotfiles/uni-druck.sh
+++ .dotfiles/uni-druck.sh
@@ -2,69 +2,69 @@
 printer=""
 ssh_target=""
 while getopts ':p:' OPTION; do
-    case "$OPTION" in
-        p)
-            case "$OPTARG" in
+	case "$OPTION" in
+		p)
+			case "$OPTARG" in
 
-                "fsr")
-                    printer=fsi-buero
-                    ssh_target=eins
-                    ;;
+				"fsr")
+					printer=fsi-buero
+					ssh_target=eins
+					;;
 
-                "oh14")
-                    printer=ohs14pr1
-                    ssh_target=uran
-                    ;;
+				"oh14")
+					printer=ohs14pr1
+					ssh_target=uran
+					;;
 
-                "fin")
-                    printer=oh14eg
-                    ssh_target=eins
-                    ;;
+				"fin")
+					printer=oh14eg
+					ssh_target=eins
+					;;
 
-                "net")
-                    printer=net
-                    ;;
+				"net")
+					printer=net
+					;;
 
-            esac
-    esac
+			esac
+	esac
 done
 shift "$(($OPTIND -1))"
 if [ "$printer" = "" ]; then
-    echo "no printer selected"
-    exit 1
+	echo "no printer selected"
+	exit 1
 fi
 if [ "$ssh_target" = "" ]; then
-    print_command="lpr -P $printer"
-    location_info="in current network"
+	print_command="lpr -P $printer"
+	location_info="in current network"
 else
-    print_command="ssh $ssh_target \"lpr -P $printer\""
-    location_info="accessing through $ssh_target"
+	print_command="ssh $ssh_target \"lpr -P $printer\""
+	location_info="accessing through $ssh_target"
 fi
 success=""
 failed=""
 while [ $# -gt 0 ]; do
-    if [ ! -e "$1" ]; then
-        failed+="\n\t- \"$1\" not found"
-        shift 1
-        continue
-    fi
-    if [ ! -f "$1" ]; then
-        failed+="\n\t- \"$1\" is not a regular file"
-        shift 1
-        continue
-    fi
-    success+="\n\t- \"$1\""
-    echo "printing $1 at $printer $location_info"
-    cat "$1" | ssh $ssh_target "lpr -P $printer"
-    #print_command="echo test"
-    #cat "$1" | eval $print_command
-    shift 1
+	if [ ! -e "$1" ]; then
+		failed+="\n\t- \"$1\" not found"
+		shift 1
+		continue
+	fi
+	if [ ! -f "$1" ]; then
+		failed+="\n\t- \"$1\" is not a regular file"
+		shift 1
+		continue
+	fi
+	success+="\n\t- \"$1\""
+	echo "printing $1 at $printer $location_info"
+	cat "$1" | ssh $ssh_target "lpr -P $printer"
+	#print_command="echo test"
+	#cat "$1" | eval $print_command
+	shift 1
 done
 echo ""
 if [ "$success" != "" ]; then
-    echo -e "successfully printed:" $success
+	echo -e "successfully printed:" $success
 fi
 if [ "$failed" != "" ]; then
-    echo -e "failed to print:" $failed
+	echo -e "failed to print:" $failed
 fi
 echo 
