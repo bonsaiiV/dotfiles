e81856bf50ff259629b1034aea0874826eb7b3d2
Bonsaiiv <dominik.riemer@udo.edu>
2025-08-17 17:35:50 +0200

    nvim: jump to diagnostics from diagnostics buffer

--- .config/nvim/lua/lsp/keybinds.lua
+++ .config/nvim/lua/lsp/keybinds.lua
@@ -10,7 +10,8 @@ vim.keymap.set(
     function()
         local count = 0
         local lines = {}
-        for k, diag in pairs(vim.diagnostic.get(0)) do
+        local diagnostics = vim.diagnostic.get(0)
+        for k, diag in pairs(diagnostics) do
             count = k
             table.insert(lines, tostring(diag.lnum + 1) .. ": " .. diag.message)
         end
@@ -18,11 +19,19 @@ vim.keymap.set(
             --lines = {"no diagnostics in file"}
             lines = {"you made a huge mistake cominig here"}
         end
+        local old_win = vim.fn.win_getid()
         local buf = vim.api.nvim_create_buf(false, true)
         vim.api.nvim_buf_set_lines(buf, 0, 0, false, lines)
-        local win = vim.api.nvim_open_win(buf, true, {relative = 'win', row=3, col=10, width=100, height=12})
-        vim.wo[win].number = false
-        vim.wo[win].relativenumber = false
-        vim.keymap.set('n', '<ESC>', function() vim.api.nvim_win_close(win,false) end, {buffer = 0})
+        local diag_win = vim.api.nvim_open_win(buf, true, {relative = 'win', row=3, col=10, width=100, height=12})
+        vim.wo[diag_win].number = false
+        vim.wo[diag_win].relativenumber = false
+        vim.keymap.set('n', '<ESC>', function() vim.api.nvim_win_close(diag_win,false) end, {buffer = 0})
+        vim.keymap.set('n', '<Enter>',
+            function()
+                local diag_index = vim.api.nvim_win_get_cursor(diag_win)[1]
+                local diag_line = diagnostics[diag_index].lnum + 1
+                vim.api.nvim_win_set_cursor(old_win, {diag_line, 0})
+                vim.api.nvim_win_close(diag_win,false)
+            end, {buffer = 0})
     end
 )
