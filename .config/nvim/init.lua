21062e5ef6aa1a4305cae8df7cf4b9d69670fdd1
Bonsaiiv <dominik.riemer@udo.edu>
2025-08-17 18:09:44 +0200

    nvim: remove formmating commands from top level init
    they are already present in filetypes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -47,30 +47,5 @@ require 'lazy'.setup('plugins', {
         notify = false,
     },
 })
-api.nvim_create_user_command('Tabs',
-    function()
-        vim.opt_local.expandtab = false
-        vim.opt_local.shiftwidth = 8
-    end,
-    {desc ='use tabs for indenting'}
-)
-api.nvim_create_user_command('Spaces',
-    function()
-        vim.opt_local.expandtab = true
-        vim.opt_local.shiftwidth = 4
-    end,
-    {desc ='use spaces for indenting'}
-)
-api.nvim_create_user_command('Scratch',
-    function()
-        vim.cmd("split")
-        vim.cmd("noswapfile hide enew")
-        vim.opt_local.buftype='nofile'
-        vim.opt_local.bufhidden='hide'
-        vim.opt_local.buflisted=false
-        vim.cmd("lcd ~")
-    end,
-    {desc ='creates a scratch buffer'}
-)
 require('filetypes')
 require('lsp')
