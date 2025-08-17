efbae173fdde2037622c94873034e9a49e00496f
Bonsaiiv <dominik.riemer@udo.edu>
2025-08-17 18:12:48 +0200

    fish: fix new terminal alias

--- .config/fish/functions/t.fish
+++ .config/fish/functions/t.fish
@@ -1,4 +1,4 @@
 function t --wraps='alacritty & && disown' --description 'alias t alacritty & && disown'
-  alacritty 2>/dev/null & && disown $argv 
-        
+	alacritty 2>/dev/null & 
+	disown
 end
