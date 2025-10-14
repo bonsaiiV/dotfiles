51edbe407b395d7d68d65d4c1edaf920a4a4cdba
Bonsaiiv <dominik.riemer@udo.edu>
2025-10-15 00:01:48 +0200

    nvim: clangd as cpp ls

--- .config/nvim/lua/lsp/clangd.lua
+++ .config/nvim/lua/lsp/clangd.lua
@@ -1,6 +1,6 @@
 vim.lsp.config['clangd'] = {
     cmd = {'clangd', '--compile-commands-dir=build'}
-    ,filetypes = {'c'}
+    ,filetypes = {'c', 'cpp'}
     ,root_markers = { '.clangd', 'build', 'bin', '.git' }
 }--]]
 
