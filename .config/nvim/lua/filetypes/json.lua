f463c8aa8c3e37eb837130c7e6fb6dad277136e0
Bonsaiiv <dominik.riemer@udo.edu>
2025-11-25 11:28:11 +0100

    nvim: json format with tabs

--- .config/nvim/lua/filetypes/json.lua
+++ .config/nvim/lua/filetypes/json.lua
@@ -3,11 +3,10 @@ vim.api.nvim_create_autocmd(
     {
         pattern = {"*.json"},
         callback = function()
-            print('test')
             vim.api.nvim_create_user_command('Format',
                 function()
                     local b_lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
-                    local formatted_json = vim.system({'jq'}, {stdin = table.concat(b_lines, '\n')}):wait()
+                    local formatted_json = vim.system({'jq', '--tab'}, {stdin = table.concat(b_lines, '\n')}):wait()
                     local lines = {}
                     for s in formatted_json.stdout:gmatch("[^\r\n]+") do
                         table.insert(lines, s)
