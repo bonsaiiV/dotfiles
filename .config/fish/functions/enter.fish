e9f2513ea1418cd42f0ad674f9a50141d3349a2b
Bonsaiiv <dominik.riemer@udo.edu>
2025-12-19 16:24:26 +0100

    fish: enter timeout for nvim session file reuse

--- .config/fish/functions/enter.fish
+++ .config/fish/functions/enter.fish
@@ -5,6 +5,8 @@ function start-tmux -d "start a tmux session in given directory"
 		set name $argv[2]
 	end
 
+	set nvim_max_session_age "-1 hour"
+
 	set tmux_env_list "-e" "SESSION_NAME=$name"
 	if [ (count $argv) -ge 3 ]
 		set session_file $argv[3]
@@ -18,17 +20,24 @@ function start-tmux -d "start a tmux session in given directory"
 				set tmux_env_list $tmux_env_list "-e" "$line"
 			end
 		end
+		if [ (jq 'has("nvim_max_session_age")' < $session_file) = true ]
+			set nvim_max_session_age "-$(jq -r '."nvim_max_session_age"' < $session_file)"
+		end
 	end
 
-	set session_file "$XDG_CACHE_HOME/sessions/$name.vim"
-	if [ -f $session_file ]
-		set nvim_param "-S $session_file"
-	else
-		set nvim_param "\."
+	set nvim_param "\."
+	set nvim_session_file "$XDG_CACHE_HOME/sessions/$name.vim"
+	if [ -f $nvim_session_file ]
+		set m_date "$(stat -c '%Y' $nvim_session_file)"
+		set cmp_date "$(date -d "$nvim_max_session_age" '+%s')"
+		if [ $m_date -gt $cmp_date ]
+			set nvim_param "-S $nvim_session_file"
+		end
 	end
 
 	tmux new-session -dc "$dir" $tmux_env_list -s $name "nvim $nvim_param"
 
+	#tmux new-window -dc "$dir" -t "$name"":"
 	tmux new-window -dc "$dir" -t "$name"":"
 	tmux attach -t $name
 end
