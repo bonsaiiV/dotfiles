d18826d775a2758c68e0106e6d41835d03e54639
Bonsaiiv <dominik.riemer@udo.edu>
2025-12-19 16:25:17 +0100

    nvim: keybind to swap between config folders

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,6 +4,16 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function ()
         local builtin = require('telescope.builtin')
+        if os.getenv('SESSION_NAME') == 'config' then
+            vim.keymap.set(
+                'n',
+                '<Leader>c',
+                function() builtin.find_files({
+                    find_command = {'ls'},
+                    })
+                end
+            )
+        end
         vim.keymap.set('n', '<Leader>g', function() builtin.live_grep() end)
         vim.keymap.set('n', '<Leader>f', function() builtin.find_files() end)
         vim.keymap.set('n', '<Leader>b', function() builtin.buffers() end)
