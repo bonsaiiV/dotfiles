4fb1b1bb2b554493cae184308d39685227738170
Bonsaiiv <dominik.riemer@udo.edu>
2026-01-02 09:01:09 +0100

    nvim: usercommand to edit config of current session

--- .config/nvim/lua/filetypes/commands.lua
+++ .config/nvim/lua/filetypes/commands.lua
@@ -13,11 +13,26 @@ vim.api.nvim_create_user_command('Spaces',
     end,
     {desc ='use spaces for indenting'}
 )
+local session_name = os.getenv('SESSION_NAME')
+if session_name then
+    vim.api.nvim_create_user_command('SessionConfig',
+        function()
+            local home_dir = os.getenv('HOME')
+            local buf = vim.api.nvim_create_buf(true, false)
+            local filename = home_dir .. '/.dotfiles/sessions/' .. session_name .. '.json'
+            vim.api.nvim_buf_set_name(buf, filename)
+            vim.api.nvim_buf_call(buf, function() vim.cmd.edit(filename) end)
+            local opts = {win = 0, vertical = true, split = 'right'}
+            vim.api.nvim_open_win(buf, true, opts)
+        end,
+        {desc ='edit the config for this session'}
+    )
+end
 vim.api.nvim_create_user_command('Scratch',
     function()
         local buf = vim.api.nvim_create_buf(false, true)
         local opts = {win = 0, vertical = true, split = 'right'}
-        local win = vim.api.nvim_open_win(buf, true, opts)
+        vim.api.nvim_open_win(buf, true, opts)
     end,
     {desc ='creates a scratch buffer'}
 )
