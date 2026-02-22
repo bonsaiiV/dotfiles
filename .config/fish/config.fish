e97ad8b6ce43084aac8a4b3f6a4242eee2092472
Bonsaiiv <dominik.riemer@udo.edu>
2026-02-22 22:28:20 +0100

    fish: use vi-mode after 4.4.0 update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
 	set -U fish_greeting
 	fish_config theme choose Dracula
+	set -g fish_key_bindings fish_vi_key_bindings
 end
 
 export CABAL_CONFIG="$XDG_CONFIG_HOME/cabal/config"
@@ -9,6 +10,7 @@ export PATH="$PATH:$HOME/.cabal/bin"
 export SHELL="/bin/fish"
 export BROWSER="/usr/bin/firefox"
 export EDITOR="/usr/bin/nvim"
+export LD_LIBRARY_PATH="/usr/local/lib"
 
 if [ "$(tty)" = "/dev/tty1" ]
 	exec sway
