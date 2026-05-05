f3f78094e62b1b38b03f3578d43fd2af1e899081
Bonsaiiv <dominik.riemer@udo.edu>
2026-05-05 08:58:10 +0200

    fish: function to sync keepassxc database with pi

---
+++ .config/fish/functions/synckeypassdb.fish
@@ -0,0 +1,23 @@
+function synckeypassdb
+	set tmp_db_dir "/tmp/keepass_dbs"
+	mkdir -p "$tmp_db_dir"
+
+	if [ (count $argv) -ge 1 ]
+		set db $argv[1]
+	else
+		set db "default"
+	end
+
+	set local_db_file "$XDG_DATA_HOME/keepass/$db"".kdbx"
+
+	if rsync "pi:/usr/local/share/keepass/$db"".kdbx" "$tmp_db_dir""/$db"".kdbx"
+		if [ ! -f  "$local_db_file" ]
+			mv "$tmp_db_dir""/$db"".kdbx" "$local_db_file"
+			return
+		end
+		cp "$local_db_file" "$local_db_file"".bkup"
+		keepassxc-cli merge --same-credentials "$local_db_file" "$tmp_db_dir""/$db"".kdbx"
+	end
+
+	rsync "$local_db_file" "pi:/usr/local/share/keepass/$db"".kdbx"
+end
