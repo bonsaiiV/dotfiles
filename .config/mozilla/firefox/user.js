22fcd552fa99d059c6a5a24b50e51af143007d82
Bonsaiiv <dominik.riemer@udo.edu>
2026-05-10 08:58:02 +0200

    firefox: sort user.js

--- .config/mozilla/firefox/user.js
+++ .config/mozilla/firefox/user.js
@@ -1,15 +1,20 @@
 // privacy
 user_pref("browser.privatebrowsing.autostart", true);
-user_pref("dom.security.https_only_mode", true);
-user_pref("services.sync.prefs.sync-seen.dom.security.https_only_mode", true);
 user_pref("privacy.resistFingerprinting", true);
-user_pref("privacy.resistFingerprinting.exemptedDomains", "*.vhs.cloud, *.netflix.com, *.nflxvideo.net, *.nflxext.com, *.fachschaften.org");
+user_pref("privacy.resistFingerprinting.exemptedDomains", "*.vhs.cloud, *.netflix.com, *.nflxvideo.net, *.nflxext.com, *.fachschaften.org, [::1], localhost");
 // user_pref("privacy.donottrackheader.enabled", true);
 user_pref("privacy.fingerprintingProtection", true);
 user_pref("privacy.query_stripping.enabled", true);
+user_pref("browser.contentblocking.category", "strict");
+
+// https only mode
+user_pref("dom.security.https_only_mode", true);
+user_pref("services.sync.prefs.sync-seen.dom.security.https_only_mode", true);
+user_pref("browser.xul.error_pages.expert_bad_cert", true);
+
+// disable telemetry
 user_pref("datareporting.healthreport.uploadEnabled", false);
 user_pref("datareporting.usage.uploadEnabled", false);
-user_pref("browser.contentblocking.category", "strict");
 
 // deactivate ai-features
 user_pref("browser.ml.chat.enabled", false);
