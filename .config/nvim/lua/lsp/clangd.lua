3b53f4f5a134e6499a88aad50c7c29f008209143
Bonsaiiv <dominik.riemer@udo.edu>
2026-06-14 17:32:15 +0200

    nvim + clangd: configure clangd separately

--- .config/nvim/lua/lsp/clangd.lua
+++ .config/nvim/lua/lsp/clangd.lua
@@ -1,7 +1,12 @@
-vim.lsp.config['clangd'] = {
+vim.lsp.config('clangd', {
     cmd = {'clangd', '--compile-commands-dir=build'}
     ,filetypes = {'c', 'cpp'}
     ,root_markers = { '.clangd', 'build', 'bin', '.git' }
-}--]]
+    ,settings = {
+        -- Don't configure here
+        -- Instead use:
+        -- file:///home/bonsaiiv/.config/clangd/config.yaml
+    },
+})--]]
 
 vim.lsp.enable('clangd')
