ef7a34dec437570a143c229de0273fb47585f642
Bonsaiiv <dominik.riemer@udo.edu>
2026-06-14 17:44:13 +0200

    nvim: reasonable autocomplete (i hope)

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,11 +1,24 @@
 require('lsp/clangd')
 require('lsp/luals')
+require('lsp/texlab')
 require('lsp/keybinds')
 
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('UserLspConfig', {}),
     callback = function(ev)
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = ev.buf })
+
+        local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+        if client:supports_method('textDocument/completion') then
+            -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+            --client.server_capabilities.completionProvider.triggerCharacters = {"<Tab>"}
+
+            vim.lsp.completion.enable(true, client.id, ev.buf, {autotrigger = false})
+
+            vim.keymap.set('i', '<Tab>', function()
+                vim.lsp.completion.get()
+            end, {})
+        end
     end,
 })
 return {}
