e84d175a7689cdf9e5cd00de2e2773ba523cbb51
Bonsaiiv <dominik.riemer@udo.edu>
2026-06-21 17:40:35 +0200

    fish: shaz-info can write year and album on tagged

--- .config/fish/functions/shaz-info.fish
+++ .config/fish/functions/shaz-info.fish
@@ -1,12 +1,22 @@
 function shaz-info -d 'add metadata to mp3 through shazam'
 	argparse 'l/album=' 'y/year=' 'f/force' -- $argv
+		if set -ql _flag_album[1]
+			set args_for_all $args_for_all -l $_flag_album[1]
+		end
+		if set -ql _flag_year[1]
+			set args_for_all $args_for_all -y $_flag_year[1]
+		end
 	for f in $argv
 		if [ ! -f "$f" ]
 			echo "$f does not exist or is not a regular file"
 			continue
 		end
 		set -ql _flag_force; or if [ -n "$(mp3info $f -p "%t" 2>/dev/null)" ]
-			echo "$f is already tagged, skipping"
+			if set -ql args_for_all
+				mp3info $f $args_for_all
+			else
+				echo "$f is already tagged, skipping"
+			end
 			continue
 		end
 
@@ -18,11 +28,7 @@ function shaz-info -d 'add metadata to mp3 through shazam'
 		set title "$(echo $response | jq ".[\"track\"][\"title\"]" -r)"
 		set artist "$(echo $response | jq ".[\"track\"][\"subtitle\"]" -r)"
 
-		set args -a $artist -t $title -g $genre
-		set -ql _flag_album[1]
-		and set args $args -l $_flag_album[1]
-		set -ql _flag_year[1]
-		and set args $args -y $_flag_year[1]
+		set args -a $artist -t $title -g $genre $args_for_all
 
 		echo "artist: $artist"
 		echo "title: $title"
